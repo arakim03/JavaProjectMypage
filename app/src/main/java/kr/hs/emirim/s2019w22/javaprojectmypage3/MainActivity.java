4004d52ff7b0f527b48ab2837bdca339c217d814
arakim03 <s2019w22@e-mirim.hs.kr>
2020-10-26 06:46:16 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2019w22/javaprojectmypage3/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emirim.s2019w22.javaprojectmypage3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.btn_change);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        changetap.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/kr/hs/emirim/s2019w22/javaprojectmypage3/changetap.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.s2019w22.javaprojectmypage3;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class changetap extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
+
+
